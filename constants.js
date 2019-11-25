f3f94f02bc05ce3018339e2e7db4db8a2f61a89e
Jen Looper <jen.looper@gmail.com>
2019-11-24 19:54:41 -0500

    prospectus

--- constants.js
+++ constants.js
@@ -2,10 +2,13 @@
 // but none of this stuff gets mutated,
 // so I'm fine with it being in this file (for now)
 
-export const REGISTRATION_URL = 'https://ti.to/frontendfoxes/vvdayus/with/zutegidximo';
-export const CFP_URL = 'https://docs.google.com/forms/d/e/1FAIpQLSeINlRZRk3QrRPJLFJAjIk_IgEiEvp6NuKDcKp4L_AILbs1zw/viewform?usp=sf_link';
-export const FAQ_URL = 'https://www.vuevixens.org/about';
-export const HEADER_IMAGE = require('./assets/images/austin-city-darken.jpeg');
+export const REGISTRATION_URL =
+  'https://ti.to/frontendfoxes/vvdayus/with/zutegidximo'
+export const CFP_URL =
+  'https://docs.google.com/forms/d/e/1FAIpQLSeINlRZRk3QrRPJLFJAjIk_IgEiEvp6NuKDcKp4L_AILbs1zw/viewform?usp=sf_link'
+export const FAQ_URL = 'https://www.vuevixens.org/about'
+export const SPONSOR_URL = './assets/vvday-us-2020.pdf'
+export const HEADER_IMAGE = require('./assets/images/austin-city-darken.jpeg')
 
 export const PRESENTERS_DATA = [
   {
@@ -13,24 +16,24 @@ export const PRESENTERS_DATA = [
     talkTitle: `"The Art of Code Comments"`,
     photo: require('./assets/images/speakers/sarah-drasner_400x400.jpg'),
     twitterUrl: `https://twitter.com/sarah_edo`,
-    keynote: true,
+    keynote: true
   },
   {
     name: `Jen Looper`,
     talkTitle: `"The Humanist's Guide to Deep Learning"`,
     photo: require('./assets/images/speakers/jen-looper_400x400.png'),
-    twitterUrl: `https://twitter.com/jenlooper`,
+    twitterUrl: `https://twitter.com/jenlooper`
   },
   {
     name: `Maria Lamardo`,
     talkTitle: `"When Life Takes Your Scrubs; npm run build"`,
     photo: require('./assets/images/speakers/maria-lamardo_400x400.jpg'),
-    twitterUrl: `https://twitter.com/MariaLamardo`,
+    twitterUrl: `https://twitter.com/MariaLamardo`
   },
   {
     name: `Diana Rodriguez`,
     talkTitle: `"Fostering Success in Tech: The DevOps culture approach"`,
     photo: require('./assets/images/speakers/diana-rodriguez-manrique_400x400.jpg'),
-    twitterUrl: `https://twitter.com/cotufa82`,
-  },
+    twitterUrl: `https://twitter.com/cotufa82`
+  }
 ]
--- pages/index.vue
+++ pages/index.vue
@@ -3,15 +3,19 @@
     <v-col no-gutters>
       <VVHeroImageContainer class="text-center header-image" :image="HEADER_IMAGE">
         <div class="content">
-          <VVLogo class="homepage-logo"/>
+          <VVLogo class="homepage-logo" />
           <h1 class="vv-heading where-header">Austin, TX</h1>
           <h1 class="vv-subheading when-header">March 5, 2020</h1>
-          <h2 class="vv-subtitle" style="margin-top: 10px;">A one-day conference presented by foxy people who identify as women.</h2>
+          <h2
+            class="vv-subtitle"
+            style="margin-top: 10px;"
+          >A one-day conference presented by foxy people who identify as women.</h2>
         </div>
       </VVHeroImageContainer>
       <section class="vv-cta-buttons">
         <VVCtaButton link :href="REGISTRATION_URL" target="_blank">Register</VVCtaButton>
-        <VVCtaButton link :href="CFP_URL" target="_blank">Give a Lightning Talk</VVCtaButton>
+        <VVCtaButton link :href="SPONSOR_URL" target="_blank">Sponsor</VVCtaButton>
+        <VVCtaButton link :href="CFP_URL" target="_blank">CFP</VVCtaButton>
         <VVCtaButton link :href="FAQ_URL">FAQ</VVCtaButton>
       </section>
       <v-col cols="8" class="content-container">
@@ -32,12 +36,18 @@
 </template>
 
 <script>
-import VVHeroImageContainer from '../components/HeroImageContainer';
-import VVCtaButton from '../components/CtaButton';
-import VVLogo from '../components/Logo';
-import VVPresenters from '../components/presenters/Presenters';
-import VVTweetTimeline from '../components/TweetTimeline';
-import { REGISTRATION_URL, CFP_URL, FAQ_URL, HEADER_IMAGE } from '../constants';
+import VVHeroImageContainer from '../components/HeroImageContainer'
+import VVCtaButton from '../components/CtaButton'
+import VVLogo from '../components/Logo'
+import VVPresenters from '../components/presenters/Presenters'
+import VVTweetTimeline from '../components/TweetTimeline'
+import {
+  REGISTRATION_URL,
+  CFP_URL,
+  FAQ_URL,
+  SPONSOR_URL,
+  HEADER_IMAGE
+} from '../constants'
 
 export default {
   components: {
@@ -48,96 +58,121 @@ export default {
     VVPresenters
   },
   computed: {
-    HEADER_IMAGE() { return HEADER_IMAGE },
-    REGISTRATION_URL() { return REGISTRATION_URL },
-    CFP_URL() { return CFP_URL },
-    FAQ_URL() { return FAQ_URL },
-  },
+    HEADER_IMAGE() {
+      return HEADER_IMAGE
+    },
+    REGISTRATION_URL() {
+      return REGISTRATION_URL
+    },
+    CFP_URL() {
+      return CFP_URL
+    },
+    FAQ_URL() {
+      return FAQ_URL
+    },
+    SPONSOR_URL() {
+      return SPONSOR_URL
+    }
+  }
 }
 </script>
 
 <style lang="scss">
-  h1 { font-size: 2.7rem; }
-  h2 { font-size: 2.0rem; }
+h1 {
+  font-size: 2.7rem;
+}
+h2 {
+  font-size: 2rem;
+}
 
-  h1, h2 {
-    font-weight: 500;
-    font-family: "Futura-CondensedMedium", "Helvetica Neue", Arial, sans-serif !important;
-    color: #34485d;
-    text-align: center;
+h1,
+h2 {
+  font-weight: 500;
+  font-family: 'Futura-CondensedMedium', 'Helvetica Neue', Arial, sans-serif !important;
+  color: #34485d;
+  text-align: center;
+}
+
+.header-image {
+  width: 100%;
+  height: 60vh;
+  min-height: 490px;
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  span,
+  div {
+    color: #fff;
+  }
+}
+
+.content {
+  padding-top: 2vh;
+}
+
+.content-container {
+  max-width: 90vw;
+  margin: 20px auto 0;
+  @media (max-width: 800px) {
   }
 
-
-  .header-image {
-    width: 100%;
-    height: 60vh;
-    min-height: 490px;
-
-    h1, h2, h3, h4, h5, h6, span, div {
-      color: #fff;
-    }
+  @media (min-width: 1200px) {
+    max-width: 880px;
   }
 
-  .content {
-    padding-top: 2vh;
-  }
-
-  .content-container {
-    max-width: 90vw;
+  & > section:not(:first-child) {
     margin: 20px auto 0;
-    @media(max-width: 800px) {}
-
-    @media(min-width: 1200px) {
-      max-width: 880px;
-    }
-
-    & > section:not(:first-child) {
-      margin: 20px auto 0;
-    }
   }
+}
 
-  .homepage-logo {
-    $width: 150px;
+.homepage-logo {
+  $width: 150px;
 
-    width: $width;
+  width: $width;
 
-    // Offset the fox logo, because the bottom of the Vue triangle should be centered
-    // and the fox tail throws it off by 20%
-    margin-left: -#{$width * .2};
-  }
+  // Offset the fox logo, because the bottom of the Vue triangle should be centered
+  // and the fox tail throws it off by 20%
+  margin-left: -#{$width * 0.2};
+}
 
-  .vv-subtitle {
-    font-family: "Futura-MediumItalic", "Helvetica Neue", Arial, sans-serif;
-    font-style: italic;
+.vv-subtitle {
+  font-family: 'Futura-MediumItalic', 'Helvetica Neue', Arial, sans-serif;
+  font-style: italic;
+  font-weight: 400;
+  max-width: 390px;
+  font-size: 1.2rem;
+  margin: 20px auto 0;
+}
+
+.vv-heading.where-header {
+  font-size: 4.2rem;
+}
+
+.vv-subheading.when-header {
+  font-size: 2.1rem;
+}
+
+.vv-heading,
+.vv-subheading {
+  &.when-header,
+  &.where-header {
+    margin-bottom: 25px;
+    font-family: 'Futura-CondensedMedium', 'Helvetica Neue', Arial, sans-serif;
     font-weight: 400;
-    max-width: 390px;
-    font-size: 1.2rem;
-    margin: 20px auto 0;
+    text-transform: uppercase;
   }
+}
 
-  .vv-heading.where-header {
-    font-size: 4.2rem;
-  }
-
-  .vv-subheading.when-header {
-    font-size: 2.1rem;
-  }
-
-  .vv-heading, .vv-subheading {
-    &.when-header, &.where-header {
-      margin-bottom: 25px;
-      font-family: "Futura-CondensedMedium", "Helvetica Neue", Arial, sans-serif;
-      font-weight: 400;
-      text-transform: uppercase;
-    }
-  }
-
-  .vv-cta-buttons {
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    height: 150px;
-    max-width: 830px;
-    margin: -80px auto 0;
-  }
+.vv-cta-buttons {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 150px;
+  max-width: 830px;
+  margin: -80px auto 0;
+}
 </style>
